e216c80fa059af4924d352504afe5cdcb71d1a6b
l <l@qq.com>
2019-06-05 11:49:48 +0800

    第二个model为正常的机器人聊天，去掉象棋，第一个model只能在27上运行，第二个model能在手机上运行

---
+++ bomberman/src/main/java/com/example/bomberman/Config.java
@@ -0,0 +1,12 @@
+package com.example.bomberman;
+
+/**
+ * author:Created by ZhangPengFei.
+ * data: 2017/12/28
+ * 配置类
+ */
+public class Config {
+    public static final String URL_KEY = "http://www.tuling123.com/openapi/api";
+    public static final String APP_KEY = "dc8443ef16bf4c00ab44c16f352edc21";//此处是你申请的Apikey
+
+}
